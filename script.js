4910c2a44e68d881ec8f68d54931a3aee1630a36
Tejal Salve <60121669+tejalsalve19@users.noreply.github.com>
2023-05-22 15:55:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let tejArr=[
 	[
 		{'one',2,'three',true,"hi"},
-		{'two',3,'four',false,"hello"},
-		{'three',4,'five',true,"bye"},
-		{'four',5,'six',false,"thanks"},
-		{'five',,'seven',true,"welcome"}
+		// {'two',3,'four',false,"hello"},
+		// {'three',4,'five',true,"bye"},
+		// {'four',5,'six',false,"thanks"},
+		// {'five',,'seven',true,"welcome"}
 	]
 ];
 console.log(tejArr);
